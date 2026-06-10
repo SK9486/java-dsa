c9f13b9559a2a9b8167465956aafb297b9bdbaf5
SK9486 <valli74appan@gmail.com>
2026-06-10 20:14:34 +0530

    Number of Islands completed

--- Hello.java
+++ Hello.java
@@ -1,39 +1,61 @@
 import java.util.*;
 
 class Hello {
-    public static void main(String[] args) {
-       ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
-        arr.add(new ArrayList<>(Arrays.asList(2,3,1)));
-        arr.add(new ArrayList<>(Arrays.asList(0)));
-        arr.add(new ArrayList<>(Arrays.asList(0,4)));
-        arr.add(new ArrayList<>(Arrays.asList(0)));
-        arr.add(new ArrayList<>(Arrays.asList(2)));
-        boolean[] visited = new boolean[arr.size()];
-        ArrayList<Integer> res = new ArrayList<>();
-        HashSet<Integer> set = new HashSet<>();
-        for(ArrayList<Integer> ar : arr){
-            for(int a : ar){
-                set.add(a);
-            }
+   public static void main(String[] args) {
+    // String[][] grid = {
+    // {"1","1","1","1","0"},
+    // {"1","1","0","1","0"},
+    // {"1","1","0","0","0"},
+    // {"0","0","0","0","0"}
+    // };
+    String[][] grid = {
+         {"1","1","0","0","0"},
+        {"1","1","0","0","0"},
+        {"0","0","1","0","0"},
+        {"0","0","0","1","1"}
+    };
+    boolean[][] visited = new boolean[grid.length][grid[0].length];
+    for(boolean[] vis : visited){
+        for(boolean v : vis){
+            System.out.print(v+" ");
         }
-        ArrayList<Integer> nodes = new ArrayList<>(set);
-        Collections.sort(nodes);
-        dfs(0,arr,visited,res,nodes);
-        for(int a : res){
-            System.out.println(a+" ");
-        }
+        System.out.println();
     }
-    public static void dfs(int curr,ArrayList<ArrayList<Integer>> adj,boolean[] visited,ArrayList<Integer> res,ArrayList<Integer> nodes){
-        int indexOfCurr = nodes.indexOf(curr);
-        ArrayList<Integer> curr_adj = adj.get(indexOfCurr);
-        if(!visited[indexOfCurr]){
-            res.add(curr);
-            visited[indexOfCurr] = true;
-            for(int a:curr_adj){
-                dfs(a, adj, visited, res, nodes);
-            }
-        }
+    // TOP BOTTOM LEFT RIGHT
+    int c =0;
+    int[] ones = findOnes(grid, visited);
+    while(ones[0] != -1 || ones[1] != -1){
+        dfs(ones[0],ones[1], grid, visited);
+        ones = findOnes(grid, visited);
+        c++;
     }
+    System.out.println("c : "+c);
 
+   }
+   public static void dfs(int i,int j,String[][] grid,boolean[][] visited){
+    if(grid[i][j].equals("1" && visited[i][j] == false){
+        System.out.println("dfs :["+i+" , "+j+"]");
+        visited[i][j] = true;
+        int[][] dirs = {{i-1,j},{i+1,j},{i,j-1},{i,j+1}};
+            for(int[] dir : dirs){
+                int a = dir[0];
+                int b = dir[1];
+                if(a>=0 && a<grid.length && b>=0 && b<grid.length){
+                    dfs(a,b,grid, visited);
+                }
+            }
+    }
+   }
+   public static int[] findOnes(String[][] grid,boolean[][] visited){
+    for(int i =0;i<grid.length;i++){
+        for(int j=0;j<grid[0].length;j++){
+            if(grid[i][j] == "1" && visited[i][j] == false){
+                System.out.println("findOnes : "+"["+i+" , "+j+"]");
+                return new int[] {i,j};
+            }
+        }
+    }
+    return new int[] {-1,-1};
+   }
 
 }
