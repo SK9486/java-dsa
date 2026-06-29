c7a61712606f8a6d0cbbf798c17c0c43f0729cf9
SK9486 <valli74appan@gmail.com>
2026-06-29 19:12:04 +0530

    first missing positive no

--- Hello.java
+++ Hello.java
@@ -2,56 +2,64 @@ import java.util.Arrays;
 
 class Hello{
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3};
-        int threshold = 5;
-        int lowerBound = 1;
-        int upperBound = findMax(nums);
-        // for(int i= lowerBound;i<=upperBound;i++){
-        //     int sum = roundSum(nums, i);
-        //     if(sum <= threshold){
-        //         System.out.println();
-        //         System.out.println("output : "+i);
-        //         break;
-        //     }
-        //     System.out.println("for d : "+i+" sum is : "+sum);
-        // }
-        System.out.println("ans : "+binarySearch(nums, lowerBound, upperBound, threshold));
-    }
-    public static int findMax(int[] arr){
-        int max = Integer.MIN_VALUE;
-        for(int a : arr){
-            if(a>max){
-                max =a;
-            }
-        }
-        return max;
-    }
-    public static int roundSum(int[] arr,int d){
-        int sum =0;
-        for(int a : arr){
-            sum += (a+(d-1))/d;
-        }
-        return sum;
-    }
-
-    public static int binarySearch(int[] arr,int lower,int higher, int threshold){
-        int i = lower;
-        int j = higher;
-        int mid  =-1;
-        int ans = -1;
-        while(i<=j){
-            mid= ((i+j)/2);
-            System.out.println("i : "+i+" j : "+j+" mid : "+mid);
-            int round = roundSum(arr, mid);
-            System.out.println("round : "+round);
-            if(round <= threshold){
-                System.out.println("founded : "+mid);
-                ans = mid;
-                j = mid - 1;
+        int[] arr = {2,2,2};
+        // int[] arr = {7,8,9,11,12};
+        printArr(arr);
+        int i =0;
+        while(i<arr.length){
+            int a_val = arr[i];
+            int a_idx = i;
+            if(a_val>0 && a_val<=arr.length){
+                System.out.println("valid : "+a_val);
+                int a_expected_idx = a_val -1;
+                if(a_expected_idx != a_idx){
+                    System.out.println("a mismatched postions :"+a_val);
+                    int b_val = arr[a_expected_idx];
+                    int b_indx = a_expected_idx;
+                    int b_expected_idx = b_val -1;
+                    if(b_expected_idx != b_indx){
+                        // swapping
+                        System.out.println("b also mismatched position "+b_val);
+                        System.out.println("swapping");
+                        System.out.println("before : a "+arr[a_idx]+" b : "+arr[b_indx]);
+                        arr[a_idx] = b_val;
+                        arr[b_indx] = a_val;
+                        System.out.println("after : a "+arr[a_idx]+" b : "+arr[b_indx]);
+                    }else{
+                        System.out.println("b is right");
+                        i++;
+                    }
+                }else{
+                    System.out.println("a is right");
+                    i++;
+                }
             }else{
-                i = mid +1;
+                System.out.println("out of range");
+                i++;
             }
         }
-        return ans;
+        int res = -1;
+        for(int j =0;j<arr.length;j++){
+            if(arr[j]-1 != j){
+                res = j+1;
+                System.out.println("founded : "+(j+1));
+                break;
+            }else{
+                System.out.println("valid : "+j+" "+arr[j]);
+            }
+        }
+        if(res == -1){
+            res = arr.length + 1;
+        }
+        return res;
+        System.out.println("final sol : "+res);
+        printArr(arr);
+
+}
+    public static void printArr(int[] arr){
+        for(int a : arr){
+            System.out.print(a+" ");
+        }
+        System.out.println();
     }
 }
