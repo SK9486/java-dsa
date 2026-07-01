205a8b69ae013b4bb6f9ed92042784b356771fac
SK9486 <valli74appan@gmail.com>
2026-07-02 00:09:44 +0530

    majority number

--- Hello.java
+++ Hello.java
@@ -1,28 +1,23 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
-class Hello {
-    public static void main(String[] args) {
-        List<String> arr = new ArrayList<>();
-        generator("", 2,0,0,arr);
+    class Hello {
+        public static void main(String[] args) {
+            int[] arr = {1,2,3,1,1,2,1};
+            int slot = 0;
+            int count = 0;
+            for(int a : arr){
+                if(slot == 0 || count < 0){
+                    slot = a;
+                    count++;
+                }else if(slot == a){
+                    count++;
+                }else{
+                    count--;
+                }
+                System.out.println("a : "+a+" slot : "+slot+" c : "+count);
+            }
     }
-
-        public static void generator(String str,int n,int open,int close,List<String> arr){
-            if(str.length() == 2 * n){
-                // if(checker(str, n)){
-                //     System.out.println("valid : "+str);
-                // }
-                // System.out.println("valid : "+str);
-                // System.out.println("open : "+open);
-                // System.out.println("close : "+close);
-                arr.add(str);
-                return;
-            }
-            if(open < n){
-                generator(str+"(", n,open+1,close,arr);
-            }
-            if(close < open){
-                generator(str+")", n,open,close+1,arr);
-            }
-        }
-}
+    }
--- Main.java
+++ Main.java
@@ -3,57 +3,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        String str1 = "neuro";
-        String str2 = "lemon";
-        // String str1 = "orange";
-        // String str2 = "energy";
-        String res = strMerger(str1, str2);
-        if(res.length() == 0){
-            System.out.println("-1");
-            return;
-        }
-        int mid = res.length() / 2;
-        System.out.println("res  : "+res);
-        System.out.println("mid : "+mid);
-        for(int i=0;i<=mid;i++){
-            int lft = mid - i;
-            int rgh = mid + i;
-            for(int j =0;j<res.length();j++){
-                if(j <= rgh){
-                    if(j == lft || j == rgh){
-                        System.out.print(res.charAt(j));
-                    }else{
-                        System.out.print("-");
-                    }
-                }
-                // else{
-                //     System.out.print(" ");
-                // }
-            }
-            System.out.println();
-        }
-    }
-
-    public static String strMerger(String str1,String str2){
-        if(str1.length() != str2.length()){
-            return "";
-        }
-        String res = "";
-        if((str1.charAt(0) == str2.charAt(str2.length() -1)) || str1.charAt(str1.length() -1 ) == str2.charAt(0)){
-            if(str1.charAt(0) == str2.charAt(str2.length() -1)){
-            String temp = str1;
-            str1 = str2;
-            str2 = temp;
-            System.out.println("str1 : "+str1);
-            System.out.println("str2 : "+str2);
-            }
-            for(int i=0;i<str1.length();i++){
-                res+=str1.charAt(i);
-            }
-            for(int j =1;j<str2.length();j++){
-                res += str2.charAt(j);
+        // int arr[] = {30,40,50,20,25};
+        int[] arr = {20,10,11,12,13,14,14,15};
+        for(int i=1;i<arr.length;i++){
+            int curr = arr[i];
+            int prev = arr[i-1];
+            if(prev > curr){
+                System.out.println("i : "+i);
+                int ans = arr.length - (arr.length - i);
+                System.out.println("ans : "+ans);
+                break;
             }
         }
-    return res;
     }
 }
