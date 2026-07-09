6c29c86402d5b4386c4d6e9ed3a528b7612c344b
SK9486 <valli74appan@gmail.com>
2026-07-09 11:36:20 +0530

    count Consecutive Ones (pod for bin)

--- Hello.java
+++ Hello.java
@@ -4,24 +4,80 @@ import java.util.HashMap;
 import java.util.List;
 
     class Hello {
-        public static void main(String[] args) {
-            int[] arr = {5,4,-1,7,8};
-            int max = Integer.MIN_VALUE;
-            int[] dp = new int[arr.length];
-            dp[0] = arr[0];
-            for(int i=1;i<arr.length;i++){
-                dp[i] = Math.max(dp[i-1]+arr[i], arr[i]);
-                printDP(dp);
-                max = Math.max(dp[i], max);
+       public static void main(String[] args) {
+    
+        int tar = 9;
+        int[] coins = {2,5,10};
+        int[][] dp = new int[coins.length][tar+1];
+        
+        for(int i =0;i<coins.length;i++){
+            for(int j = 0;j<tar+1;j++){
+                // System.out.print("("+i+" , "+j+") ");
+                if(j == 0){
+                    dp[i][j] = 0;
+                }else{
+                    if(i==0 &&coins[i] == 1){
+                        dp[i][j] = j;
+                    // System.out.println("dp[i,j] : "+dp[i][j]);
+                    }
+                    else if(i == 0 && coins[i] != 1){
+                        if(j % coins[i] == 0){
+                            dp[i][j] = j/coins[i];
+                        }else{
+                            dp[i][j] = Integer.MAX_VALUE;
+                        }
+                    }else{
+                        if(coins[i] > j){
+                            dp[i][j] = dp[i-1][j];
+                            // System.out.println(j+" <  "+coins[i]);
+                        }else{
+                            int excluded =dp[i-1][j];
+                            int included;
+                            if(dp[i][Math.abs(coins[i] - j)] != Integer.MAX_VALUE){
+                                included = 1 + dp[i][coins[i] - j];
+                            }else{
+                                included = Integer.MAX_VALUE;
+                            }
+                            dp[i][j] = Math.min(included, excluded);
+                        }
+                    }
+                }
             }
-            System.out.println("max : "+max);
+            // System.out.println();
         }
-
-        public static void printDP(int[] arr){
-            System.out.print("dp : ");
-            for(int a : arr){
-                System.out.print(a+" ");
+        printDP(dp);
+        backtracking(dp,coins);
+        // int last = dp[coins.length -1][tar];
+        // if(last > coins.length){
+            // return -1;
+        // }
+        // return last;
+        // dp[coins.length -1][tar] > coins.length ? return -1 : return dp[coins.length -1][tar];
+        // System.out.println("last : "+last);
+       }
+    public static void printDP(int[][] dp){
+        for(int[] a : dp){
+            for(int b : a){
+                System.out.print(b+" ");
             }
             System.out.println();
         }
+    }
+    // backtracking you must do next
+    public static void backtracking(int[][] dp,int[] coins){
+        int i = dp.length -1;
+        int j = dp[0].length -1;
+        int last = dp[i][j];
+        System.out.println(last);
+        System.out.println("i : "+i+" j : "+j);
+        while(dp[i-1][j] == dp[i][j]){
+            System.out.println(dp[i][j]);
+            System.out.println("i : "+i+" j : "+j);
+            i = i-1;
+        }
+        int new_i = j-coins[i];
+        i =  new_i;  
+        System.out.println("i : "+i+" j : "+j);
+        System.out.println(dp[i][j]);
+    }
     }
--- Main.java
+++ Main.java
@@ -3,51 +3,38 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        int n = 4;
-        int m = 6;
-        String[] board = {
-    "e-y-rk",
-    "xi----",
-    "--atq",
-    "-m--pa"
-};
-// String[] board = {
-//     "uwwr-kb--",
-//     "mq-hocqi-",
-//     "--g------",
-//     "-v-v-b---",
-//     "h-xr--s-o",
-//     "p-jbugt-s",
-//     "ec-cs----"
-// };
-char opr = 'L';
-String out = "";
-for(String str : board){
-    if(opr == 'L'){
-        out += getFirstAlpha(str);
-        System.out.println("out : "+out);
-    }else{
-        out += getLastAlpha(str);
-        System.out.println("out : "+out);
+        int n = 0;
+        String binOfN = intToBin(n);
+        int res = countConsecutiveOnes(binOfN);
+        System.out.println("res : "+res);
     }
-}
+    public static String intToBin(int n){
+        int rem = -1;
+        int curr = n;
+        String str = "";
+        while(curr != 0){
+            rem = curr % 2;
+            curr = curr / 2;
+            str+= String.valueOf(rem);
+            System.out.println("rem : "+rem);
+        }
+        System.out.println("str : "+str);
+        return str;
     }
-
-    public static char getFirstAlpha(String str){
-        for(int i=0;i<str.length();i++){
-            if(str.charAt(i) != '-' && str.charAt(i) != ' '){
-                return str.charAt(i);
+    public static int countConsecutiveOnes(String str){
+        int p2;
+        int max = 0;
+        for(int p1 =0;p1<str.length();p1++){
+            if(str.charAt(p1) == '1'){
+                p2 = p1;
+                while(p2<str.length() && str.charAt(p2) == '1'){
+                    p2++;
+                }
+                System.out.println("len : "+(p2-p1));
+                max = Integer.max(max, p2-p1);
+                p1 = p2;
             }
         }
-        return ' ';
+        return max;
     }
-    public static char getLastAlpha(String str){
-        for(int i=str.length()-1;i>=0;i--){
-            if(str.charAt(i) != '-' && str.charAt(i) != ' '){
-                return str.charAt(i);
-            }
-        }
-        return ' ';
-    }
-
 }
