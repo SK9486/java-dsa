74c0b484a33e597026fdb73f57118fd43ddc0109
SK9486 <valli74appan@gmail.com>
2026-07-10 12:12:09 +0530

    patten prininting with two characters

--- Main.java
+++ Main.java
@@ -1,40 +1,28 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args){
-        int n = 0;
-        String binOfN = intToBin(n);
-        int res = countConsecutiveOnes(binOfN);
-        System.out.println("res : "+res);
-    }
-    public static String intToBin(int n){
-        int rem = -1;
-        int curr = n;
-        String str = "";
-        while(curr != 0){
-            rem = curr % 2;
-            curr = curr / 2;
-            str+= String.valueOf(rem);
-            System.out.println("rem : "+rem);
-        }
-        System.out.println("str : "+str);
-        return str;
-    }
-    public static int countConsecutiveOnes(String str){
-        int p2;
-        int max = 0;
-        for(int p1 =0;p1<str.length();p1++){
-            if(str.charAt(p1) == '1'){
-                p2 = p1;
-                while(p2<str.length() && str.charAt(p2) == '1'){
-                    p2++;
+public class Main{
+    public static void main(String[] args) {
+        int n = 6;
+        char ch1 = '@';
+        char ch2 = '%';
+        int left;
+        int rght;
+        int mid = (((n-1)*2)+1)/2;
+        System.out.println("mid : "+mid);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<(((n-1)*2)+1);j++){
+                left = mid - i;
+                rght = mid + i;
+                if(j>=left && j<=rght){
+                    // System.out.print("* ");
+                    if(j % 2 != 0){
+                        System.out.print(ch1+" ");
+                    }else{
+                        System.out.print(ch2+" ");
+                    }
+                }else{
+                    System.out.print("+ ");
                 }
-                System.out.println("len : "+(p2-p1));
-                max = Integer.max(max, p2-p1);
-                p1 = p2;
             }
+            System.out.println();
         }
-        return max;
     }
 }
