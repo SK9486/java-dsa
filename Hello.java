1a63bbabccebbe373a71d0fae7887edbd2e6a0e7
SK9486 <valli74appan@gmail.com>
2026-07-12 20:07:05 +0530

    Remove Duplicates from Sorted Array

--- Hello.java
+++ Hello.java
@@ -2,31 +2,24 @@ import java.util.Arrays;
 
 class Hello{
     public static void main(String[] args) {
-        // int[] nums = {2,4,6,8,5};
-        // int tar = 10;
-        int[] nums = {25,20,25,30,15,45,5};
-        int tar = 50;
-        Arrays.sort(nums);
-        int p1  = 0;
-        int p2 = nums.length -1;
-        // for(int a : nums){
-        //     System.out.print(a+" ");
-        // }
-        System.out.println();
-        while(p1 < p2 && p1<nums.length && p2<nums.length){
-            int sum = nums[p1]+nums[p2];
-            // System.out.println("p1 : "+p1);
-            // System.out.println("p2 : "+p2);
-            // System.out.println("sum : "+sum);
-            if(sum == tar){
-                System.out.println(nums[p1]+" "+nums[p2]);
+        int[] arr = {0,0,1,1,1,2,2,3,3,4};
+        int p1 = 0;
+        int p2 = 0;
+        while(p1<arr.length && p2<arr.length){
+            if(arr[p1] != arr[p2]){
+                System.out.println("mimatch occures");
                 p1++;
-                p2--;
-            }else if(sum > tar){
-                p2--;
-            }else{
-                p1++;
+                arr[p1] = arr[p2];
             }
+            System.out.println("p1 : "+arr[p1]);
+            System.out.println("p2 : "+arr[p2]);
+            p2++;
         }
+        System.out.println("p1 : "+p1);
+        System.out.println("p2 : "+p2);
+        for(int a : arr){
+            System.out.print(a+" ");
+        }
+        System.out.println();
     }
 }
