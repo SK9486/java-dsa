b288b1bc303d634ba7c2c173830b487c6e50deb2
SK9486 <valli74appan@gmail.com>
2026-07-19 14:36:51 +0530

    Set Matrix Zeroes optmized

--- Hello.java
+++ Hello.java
@@ -3,54 +3,35 @@ import java.util.Arrays;
 class Hello{
     public static void main(String[] args) {
         int[][] mat = {{0,1,2,0},{3,4,5,2},{1,3,1,5}};
-        boolean[][] trace = new boolean[mat.length][mat[0].length];
-        for(boolean[] arr : trace){
-            for(boolean ar : arr){
-                System.out.print(ar+" ");
-            }
-            System.out.println();
-        }
+        boolean[] col_tr = new boolean[mat[0].length];
+        boolean[] row_tr = new boolean[mat.length];
         for(int i=0;i<mat.length;i++){
-            for(int j=0;j<mat[i].length;j++){
+            for(int j =0;j<mat[i].length;j++){
                 if(mat[i][j] == 0){
-                    System.out.println("i : "+i+"j : "+j+" mat[i][j] : "+mat[i][j]);
-                    trace[i][j] = true;
-                    make_it_zero(i,j,trace);
+                    col_tr[j] = true;
+                    row_tr[i] = true;
                 }
             }
         }
-         for(boolean[] arr : trace){
-            for(boolean ar : arr){
-                System.out.print(ar+" ");
-            }
-            System.out.println();
-        }
-        replace_with_zero(mat,trace);
+        replace_with_zero(mat, col_tr, row_tr);
+        printArr(mat);
 
-        for(int[] ar :mat){
+    }
+    public static void replace_with_zero(int[][] mat,boolean[] col,boolean[] row){
+        for(int i=0;i<mat.length;i++){
+            for(int j =0;j<mat[i].length;j++){
+                if(col[j] == true || row[i] == true ){
+                    mat[i][j] = 0;
+                }
+            }
+        }
+    }
+    public static void printArr(int[][] mat){
+        for(int[] ar: mat){
             for(int a : ar){
                 System.out.print(a+" ");
             }
             System.out.println();
-        }
-    }
-    public static void make_it_zero(int a,int b,boolean[][] mat){
-        for(int i=0;i<mat.length;i++){
-            for(int j=0;j<mat[i].length;j++){
-                if((a == i || b == j) && mat[i][j] != true){
-                    System.out.println("i : "+i+"j : "+j+" = "+"-1");
-                    mat[i][j] = true;
-                }
-            }
-        }
-    }
-    public static void replace_with_zero(int[][] mat,boolean[][] trace){
-        for(int i=0;i<mat.length;i++){
-            for(int j=0;j<mat[i].length;j++){
-                if(trace[i][j] == true && mat[i][j] != 0){
-                    mat[i][j] = 0;
-                }
-            }
         }
     }
 }
