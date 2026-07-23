13e47402235e36e1a96ebc03633e05ee26884fd5
SK9486 <valli74appan@gmail.com>
2026-07-23 14:04:14 +0530

    RHP D - Many Repunit Sum  unoptimized

--- Hello.java
+++ Hello.java
@@ -2,23 +2,19 @@ import java.util.*;
 
 class Hello{
     public static void main(String[] args) {
-    int[] nums ={-1,0,3,5,9,12};
-    int tar = 3;
-    int left_idx = 0;
-    int right_idx = nums.length -1;
-    int left;
-    int right;
-    while(left_idx + 1 <right_idx){
-        int mid_idx = (left_idx + right_idx)/2;
-        int mid = nums[mid_idx];
-        left = nums[left_idx];
-        right = nums[right_idx];
-        if(tar > mid){
-            left_idx = mid_idx;
-        }else{
-            right_idx = mid_idx;
+        int n = 3;
+        long sum = 0;
+        int[] arr = {15,10,15};
+        for(int a : arr){
+            System.out.println("for : "+a);
+            String str = "";
+            for(int i=0;i<a;i++){
+                str+="1";
+            }
+            long rep = Long.parseLong(str);
+            sum+=rep;
+            System.out.println("rep : "+rep);
+            System.out.println("sum : "+sum);
         }
-    }
-    return tar == left ? left_idx : tar == right ? right_idx : -1;
     }
 }
