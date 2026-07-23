4d75f458d3df6ef284e39bb0df6a7c5c34f39123
SK9486 <valli74appan@gmail.com>
2026-07-23 11:27:33 +0530

    RHP Not All Covered  optmized

--- Hello.java
+++ Hello.java
@@ -1,37 +1,44 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 class Hello{
     public static void main(String[] args) {
-        int[][] mat = {{0,1,2,0},{3,4,5,2},{1,3,1,5}};
-        boolean[] col_tr = new boolean[mat[0].length];
-        boolean[] row_tr = new boolean[mat.length];
-        for(int i=0;i<mat.length;i++){
-            for(int j =0;j<mat[i].length;j++){
-                if(mat[i][j] == 0){
-                    col_tr[j] = true;
-                    row_tr[i] = true;
-                }
-            }
-        }
-        replace_with_zero(mat, col_tr, row_tr);
-        printArr(mat);
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter value of N and M :");
+    int N = sc.nextInt();
+    int M = sc.nextInt();
+    int[] walls = new int[N+2];
+    System.out.println(" N : "+N);
+    for(int wal : walls){
+      System.out.print(wal+" ");
+    }
 
+    for(int i=0;i<M;i++){
+        int st = sc.nextInt();
+        int ed = sc.nextInt();
+        // System.out.println(st+" "+ed);
+        walls[st] = walls[st] +1;
+        walls[ed+1] = walls[ed+1] - 1;
     }
-    public static void replace_with_zero(int[][] mat,boolean[] col,boolean[] row){
-        for(int i=0;i<mat.length;i++){
-            for(int j =0;j<mat[i].length;j++){
-                if(col[j] == true || row[i] == true ){
-                    mat[i][j] = 0;
-                }
-            }
+    for(int wal : walls){
+      System.out.print(wal+" ");
+    }
+
+    int sum = 0;
+    int min = Integer.MAX_VALUE;
+    for(int i=0;i<walls.length;i++){
+        sum += walls[i];
+        walls[i] = sum;
+        if(walls[i] < min && i > 0 && i <= N){
+            min = walls[i];
         }
     }
-    public static void printArr(int[][] mat){
-        for(int[] ar: mat){
-            for(int a : ar){
-                System.out.print(a+" ");
-            }
-            System.out.println();
-        }
+    System.out.println();
+    for(int wal : walls){
+      System.out.print(wal+" ");
+    }
+    System.out.println("min : "+min);
+
+
     }
 }
--- Main.java
+++ Main.java
@@ -1,25 +1,22 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args) {
-        int[] arr = {1,8,6,2,5,4,8,3,7};
-        int p1 = 0;
-        int p2 = arr.length -1;
-        int maxArea = Integer.MIN_VALUE;
-        while(p1<=p2){
-            int breath = p2 - p1;
-            System.out.println("breath : "+breath);
-            int height = Math.min(arr[p1], arr[p2]);
-            System.out.println("height : "+height);
-            int area = breath * height;
-            if(area > maxArea){
-                maxArea = area;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n value : ");
+        int n = sc.nextInt();
+        oddNoOptimized(n);
+    }
+    public static void oddNo(int n){
+        for(int i=1;i<=n;i++){
+            if(i % 2 != 0){
+                System.out.println(i);
             }
-            System.out.println("area : "+area);
-            System.out.println("max Area : "+maxArea);
-            if(arr[p2] < arr[p1]){
-                p2--;
-            }else{
-                p1++;
-            }
+        }
+    }
+    public static void oddNoOptimized(int n){
+        for(int i=1;i<=n;i+=2){
+                System.out.println(i);
         }
     }
 }
