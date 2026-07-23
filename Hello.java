ce02ba5de7c264ffd7966cfa775a7ad312d1d2f3
SK9486 <valli74appan@gmail.com>
2026-07-23 13:43:14 +0530

    RHP Binary Search optimized

--- Hello.java
+++ Hello.java
@@ -1,47 +1,24 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 class Hello{
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-
-    // System.out.println("Enter value of N and M :");
-
-    int N = sc.nextInt();
-    int M = sc.nextInt();
-    int[] walls = new int[N+2];
-    // System.out.println(" N : "+N);
-    // for(int wal : walls){
-    //   System.out.print(wal+" ");
-    // }
-
-    for(int i=0;i<M;i++){
-        int st = sc.nextInt();
-        int ed = sc.nextInt();
-        // System.out.println(st+" "+ed);
-        for(int k=st;k<ed;k++){
-            walls[k] = walls[k] +1;
+    int[] nums ={-1,0,3,5,9,12};
+    int tar = 3;
+    int left_idx = 0;
+    int right_idx = nums.length -1;
+    int left;
+    int right;
+    while(left_idx + 1 <right_idx){
+        int mid_idx = (left_idx + right_idx)/2;
+        int mid = nums[mid_idx];
+        left = nums[left_idx];
+        right = nums[right_idx];
+        if(tar > mid){
+            left_idx = mid_idx;
+        }else{
+            right_idx = mid_idx;
         }
     }
-    for(int wal : walls){
-      System.out.print(wal+" ");
-    }
-
-    int sum = 0;
-    int min = Integer.MAX_VALUE;
-    for(int i=0;i<walls.length;i++){
-        sum += walls[i];
-        walls[i] = sum;
-        if(walls[i] < min && i > 0 && i <= N){
-            min = walls[i];
-        }
-    }
-    // System.out.println();
-    // for(int wal : walls){
-    //   System.out.print(wal+" ");
-    // }
-    // System.out.println("min : "+min);
-
-
+    return tar == left ? left_idx : tar == right ? right_idx : -1;
     }
 }
