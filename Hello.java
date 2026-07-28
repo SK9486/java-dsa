ffccfab5b5a64687793e520ad8e2e60480941db7
SK9486 <valli74appan@gmail.com>
2026-07-29 05:22:45 +0530

    2D searching 2 (leetcode)

--- Hello.java
+++ Hello.java
@@ -1,31 +1,50 @@
+import java.util.Arrays;
+
 class Hello{
     public static void main(String[] args) {
-        String st = "omega";
-        int mid = st.length()/2;
-        // String st = "killer";
-        // int mid = st.length()/2;
-        System.out.println("mid : "+mid);
-        int p2 = -1;
-        int p1 = st.length();
-        System.out.println("p1 : "+p1);
-        System.out.println("p2 : "+p2);
-        for(int i=0;i<st.length();i++){
-            p2++;
-            p1--;
-            for(int j=0;j<st.length();j++){
-                if(i == mid){
-                    System.out.print(st.charAt(j)+" ");
-                }else{
-                    if( j == mid){
-                            System.out.print(st.charAt(p2)+" ");
-                    }else if(i == j || i+j == st.length() -1){
-                            System.out.print(st.charAt(p1)+" ");
-                    }else{
-                            System.out.print("* ");
-                    }
-                }
+        // int[][] mat = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+        // int tar = 60;
+        int[][] mat = {
+            {1, 4, 7, 11, 15},
+            {2, 5, 8, 12, 19},
+            {3, 6, 9, 16, 22},
+            {10, 13, 14, 17, 24},
+            {18, 21, 23, 26, 30}
+        };
+        int n = mat.length;
+        int m = mat[0].length;
+        int i = 0;
+        int j = m -1;
+        while(j>=0 && i < m){
+            int curr = mat[i][j];
+            System.out.println("curr : "+curr);
+            if(curr > tar){
+                j--;
+            }else if(curr < tar){
+                i++;
+            }else{
+                System.out.println("founded");
+                break;
+            }
         }
-            System.out.println();
+    };
+
+    public static boolean binarySearch(int[] arrs,int i,int j,int tar){
+        while (i <= j) {
+            int mid_idx = (i + j) / 2;
+            int mid = arrs[mid_idx];
+            // System.out.println();
+            if (mid > tar) {
+                j = mid_idx - 1;
+                // System.out.println("j : " + j);
+            } else if (mid < tar) {
+                i = mid_idx + 1;
+                // System.out.println("i : " + i);
+            } else {
+                // System.out.println("found : " + mid);
+                return true;
+            }
         }
+        return false;
     }
 }
