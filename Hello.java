c08e40755d38360665dd6bcf5dc99fc7ea58b5f0
SK9486 <valli74appan@gmail.com>
2026-07-29 09:44:07 +0530

    C - Rotation (bruthforce sol)

--- Hello.java
+++ Hello.java
@@ -1,36 +1,34 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 import java.util.Stack;
 
 class Hello{
     public static void main(String[] args) {
-        Stack<Character> st = new Stack<>();
-        char[] ch = {'a','b'};
-        int N = 2;
-        bt_dfs(st, ch, N);
+        String str = "dsuccxulnl";
+        StringBuilder sb = new StringBuilder(str);
+        Scanner sc = new Scanner(System.in);
+        int n = 8;
+        while(n>0){
+            int q = sc.nextInt();
+            int x = sc.nextInt();
+            sb = quryExecutor(sb,q,x);
+            n--;
+        }
     }
-
-    public static void bt_dfs(Stack<Character> path,char [] arr,int N){
-        if(path.size() == N){
-            System.out.println("path : ");
-            arrPrinter(path);
-            return;
+    public static StringBuilder quryExecutor(StringBuilder sb,int q,int x){
+        if(q == 2){
+            System.out.println(sb.toString().charAt(x-1));
+        }else{
+            for(int i=x-1;i>=0;i--){
+               int lastindex = sb.length() -1;
+               char lastChar = sb.toString().charAt(lastindex);
+               sb.deleteCharAt(lastindex);
+               sb.insert(0, lastChar);
+               System.out.println("string : "+sb.toString());
+            }
         }
-
-        for(char ch : arr){
-            path.push(ch);
-            System.out.println("ch added : "+ch);
-            System.out.println("path : ");
-            arrPrinter(path);
-            bt_dfs(path, arr, N);
-            System.out.println("poped : "+path.pop());
-        }
-
-    }
-    public static void arrPrinter(Stack<Character> chars){
-        for(Character h : chars){
-            System.out.print(h+" ");
-        }
-        System.out.println();
+        return sb;
+        
     }
 }
---
+++ StringBulider.java
@@ -0,0 +1,6 @@
+/**
+ * StringBulider
+ */
+public class StringBulider {
+
+}
