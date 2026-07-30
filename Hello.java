7d7515c80eb82f43aa95836d90152026a6e6f9d9
SK9486 <valli74appan@gmail.com>
2026-07-30 23:34:58 +0530

    1689. Partitioning Into Minimum Number Of Deci-Binary Numbers (UNOPTIMIZED WITH BUGS)

--- Hello.java
+++ Hello.java
@@ -2,83 +2,60 @@ import java.util.*;
 
 class Main {
     public static void main(String[] args) {
-        int n_no_nodes =4 ;
-        int m_no_edge = 2;
-        int start_node = 1;
-
-        ArrayList<Integer> arr1 = new ArrayList<>(Arrays.asList(1,2));
-        ArrayList<Integer> arr2 = new ArrayList<>(Arrays.asList(1,3));
-        ArrayList<ArrayList<Integer>> nodes = new ArrayList<>(Arrays.asList(arr1,arr2));
-        HashMap<Integer,ArrayList<Integer>> map = new HashMap<>();
-
-        // Intializing the map with nodes
-        for(int i=1;i<=n_no_nodes;i++){
-            map.put(i,new ArrayList<>());
-        }
-
-        // Bulding the map with edges
-
-        for(ArrayList<Integer> node : nodes){
-            ArrayList<Integer> val = map.get(node.get(0));
-            val.add(node.get(1));
-            map.put(node.get(0),val);
-
-        }
-
-        printMap(map);
-        ArrayList<ArrayList<Integer>> res = bfs(map, start_node);
-        HashMap<Integer,Integer> ansMap = new HashMap<>();
-        for(int i=1;i<=n_no_nodes;i++){
-            ansMap.put(i,0);
-        }
-        ans_weights(ansMap,res,6);
-        for(Map.Entry<Integer,Integer> enty : ansMap.entrySet()){
-            System.out.println(enty.getKey()+" "+enty.getValue());
-        }
-
-
-        // 4 2
-        // 1 2
-        // 1 3
-        // 1
+//         char[][] maze = {
+//     {'+', '+', '.', '+'},
+//     {'.', '.', '.', '+'},
+//     {'+', '+', '+', '.'}
+// };
+char[][] maze = {
+    {'+', '+', '+'},
+    {'.', '.', '.'},
+    {'+', '+', '+'}
+};
+// char[][] maze = {
+//     {'.','+'}
+// };
+        int[] standing_point = {1,0};
+         System.out.println(bfs(standing_point[0], standing_point[1], maze));
     }
 
-    public static void printMap(HashMap<Integer,ArrayList<Integer>> map){
-        for(Map.Entry<Integer,ArrayList<Integer>> enty : map.entrySet()){
-            System.out.println(enty.getKey()+" "+enty.getValue());
-        }
-    }
-    public static ArrayList<ArrayList<Integer>>  bfs(HashMap<Integer,ArrayList<Integer>> map ,int start_node){
-        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
-        ArrayList<Integer> arr;
-        Deque<Integer> dq = new LinkedList<>();
-        dq.addLast(start_node);
-        int size = dq.size();
+
+    public static int bfs(int x,int y,char[][] maze){
+        int c= 0;
+        System.out.println(maze.length);
+        System.out.println(maze[0].length);
+        Deque<int[]> dq = new LinkedList<>();
+        dq.addLast(new int[] {x,y});
+        int size = 0;
         while(!dq.isEmpty()){
-            arr = new ArrayList<>();
-            for(int i=0;i<size;i++){
-                int poped = dq.removeFirst();
-                arr.add(poped);
-                for(int a : map.get(poped)){
-                    dq.add(a);
+            for(int k=0;k<size;k++){
+                int[] pos = dq.removeFirst();
+                int i = pos[0];
+                int j = pos[1];
+                System.out.println(i+" "+j);
+                maze[i][j] = '+';
+                int[][] dirs = {
+                    { i - 1, j },
+                    { i + 1, j },
+                    { i, j - 1 },
+                    { i, j + 1 }
+                };
+            for(int[] dir : dirs){
+                int a = dir[0];
+                int b = dir[1];
+                if(a>=0 && a < maze.length && b>=0 && b<maze[0].length && maze[a][b] == '.'){
+                    c++;
+                    if((a == 0 || a ==  maze.length - 1) || (b == 0 || b == maze[0].length -1) ){
+                         System.out.println("Breaked "+c);
+                         return c;
+                    }
+                    dq.add(new int[] {a,b});
                 }
             }
-            size = dq.size();
-            res.add(arr);
-        }
-        return res;
-    }
-
-    public static ArrayList<Integer> ans_weights(HashMap<Integer,Integer> map,ArrayList<ArrayList<Integer>> res,int weight){
-        ArrayList<Integer> ans = new ArrayList<>();
-        for(int i=0;i<res.size();i++){
-            for(int j=0;j<res.get(i).size();j++){
-                System.out.println("i : "+i+" j : "+j+" = "+res.get(i).get(j));
-                int key = res.get(i).get(j);
-                int val = weight * i;
-                map.put(key, val);
             }
+            size = dq.size();
         }
-        return ans;
+        return -1;
+
     }
 }
--- Main.java
+++ Main.java
@@ -1,17 +1,53 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 class Main{
     public static void main(String[] args) {
-        int[] arrs = {2,3,6,7};
-        Arrays.sort(arrs);
-    }
-    public static void backTracking(int tar,int[] arr,int sum){
-        if(sum > tar){
-            return;
+        String nums = "9223372036854775808";
+        ArrayList<String> base_arr = new ArrayList<>();
+        for(int i=0;i<nums.length();i++){
+            if( i == 0){
+                base_arr = build_base_arr(Character.getNumericValue(nums.charAt(i)),nums.length());
+            }else{
+                updateing_arry(Character.getNumericValue(nums.charAt(i)), i, base_arr);
+            }
         }
-        if(sum == tar){
-            System.out.println("FOUNDED : ");
-            return;
+        System.out.println(base_arr);
+    }
+    public static ArrayList<String> build_base_arr(int n,int len){
+        ArrayList<String> baseArr = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            long ele = (long)Math.pow(10L,len-1);
+            baseArr.add(String.valueOf(ele));
+        }
+        return baseArr;
+    }
+    public static ArrayList<String> updateing_arry(int n,int pos,ArrayList<String> base_arr){
+        for(int i=0;i<n;i++){
+            if(i > base_arr.size() - 1){
+                System.out.println("i : "+i+" n : "+n);
+                addOnes(n-i, base_arr);
+                break;
+            }
+            String curr = base_arr.get(i);
+            String updated = update(curr,pos);
+            base_arr.set(i,updated);
+            System.out.println("curr : "+curr);
+        }
+        return base_arr;
+    }
+
+    public static String update(String str,int pos){
+        char[] chrs = str.toCharArray();
+        chrs[pos] = '1';
+        System.out.println(chrs);
+        return new String(chrs);
+    }
+
+    public static void addOnes(int n,ArrayList<String> baseArr){
+        for(int i=0;i<n;i++){
+            baseArr.add("1");
         }
     }
 }
