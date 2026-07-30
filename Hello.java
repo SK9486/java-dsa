f01fd548087bc962952145076922393424ca0897
SK9486 <valli74appan@gmail.com>
2026-07-30 12:17:33 +0530

    994. Rotting Oranges (unoptimised)

--- Hello.java
+++ Hello.java
@@ -2,38 +2,88 @@ import java.util.*;
 
 class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        int[][] grid = {
+            {2,1,1},
+            {0,1,1},
+            {1,0,1}
+        };
+        int c_max = Integer.MIN_VALUE;
+        boolean[][] tracker = new boolean[grid.length][grid[0].length];
+        ArrayList<ArrayList<Integer>> rootedPositons = findRottenOrange(grid, tracker);
+        System.out.println("counter : "+bfs(grid, tracker,rootedPositons,0));
 
-        int testCases = sc.nextInt();
 
-        for (int t = 1; t <= testCases; t++) {
-            System.out.println("Case " + t + ":");
-
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-
-            int[] arr = new int[n];
-
-            for (int k = 0; k < n; k++) {
-                arr[k] = sc.nextInt();
+        for(int[] gr : grid){
+            for(int g : gr ){
+                System.out.print(g+" ");
             }
-            for (int i = 0; i < m; i++) {
-                int q = sc.nextInt();
+            System.out.println();
+        }
 
-                if (q == 1) {
-                    int old = sc.nextInt();
-                    int ele = sc.nextInt();
+    }
+    public static int bfs(int[][] grid, boolean[][] tracker,ArrayList<ArrayList<Integer>> positions,int c) {
+        System.out.println("BFS");
+        Deque<ArrayList<Integer>> dq = new LinkedList<>();
+        for(ArrayList<Integer> arr : positions ){
+            dq.addLast(arr);
+        }
+        int size = dq.size();
+        System.out.println("size : "+size);
+        while(!dq.isEmpty()){
+            // System.out.println("l "+l+" r : "+r);
+            for(int k=0;k<size;k++){
+                ArrayList<Integer> pos = dq.removeFirst();
+                int l=pos.get(0);
+                int r=pos.get(1);
+               int[][] dirs = {
+                    { l - 1, r },
+                    { l + 1, r },
+                    { l, r - 1 },
+                    { l, r + 1 }
+            };
+            for(int[] dir : dirs){
+                int a = dir[0];
+                int b = dir[1];
+                if(a>=0 && a <grid.length && b >=0 && b <grid[0].length && grid[a][b] == 1 && tracker[a][b] == false){
+                    System.out.println("["+a+" "+b+"]");
+                    tracker[a][b] = true;
+                    grid[a][b] = 2;
+                    dq.add(new ArrayList<>(Arrays.asList(a,b)));
+                    // bfs(grid, tracker, c+counter, a,b);
+                }
+            }
+            }
+            size = dq.size();
+            if(size > 0){
+                c++;
+            }
+            System.out.println("size : "+size);
+            System.out.println("C : "+c);
+        }
+        return c;
 
-                    for (int j = 0; j < arr.length; j++) {
-                        if (arr[j] == old) {
-                            arr[j] = ele;
-                        }
-                    }
-                } else {
-                    int idx = sc.nextInt();
-                    System.out.println(arr[idx - 1]);
+    }
+    public static ArrayList<ArrayList<Integer>> findRottenOrange(int[][] grid,boolean[][] tracker){
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[i].length;j++){
+                if(grid[i][j] == 2 && tracker[i][j] == false){
+                    tracker[i][j] = true;
+                    res.add(new ArrayList<>(Arrays.asList(i,j)));
                 }
             }
         }
+        return res;
+    }
+
+    public static boolean checkIfFreshOrangeExists(int[][] grid){
+        for(int[] gr: grid){
+            for(int g : gr){
+                if(g == 1){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
--- Main.java
+++ Main.java
@@ -1,59 +1,17 @@
-// import java.util.*;
+import java.util.Arrays;
 
-// public class Main{
-//     class Node {
-//         int data;
-//         Node leftNode;
-//         Node rightNode;
-//         int startInterval;
-//         int endInterval;
-//         Node(int data,Node left,Node right,int st,int ed){
-//             this.data = data;
-//             this.leftNode = left;
-//             this.rightNode = right;
-//             this.startInterval = st;
-//             this.endInterval = ed;
-//         }
-//     }
-
-//     class SegmentTree{
-//         Node root;
-//         public void printNode(){
-
-//         }
-//     }
-//     public static void main(String[] args) {
-//         int n = 8;
-//         int[] arr = {3,2,4,5,1,1,5,3};
-//         Node leftNode = new Node(3, null, null, 0, 0);
-//         Node rightNode = new Node(2, null, null, 1, 1);
-//         Node parent = new Node(Math.min(leftNode.data,rightNode.data), leftNode,rightNode,leftNode.startInterval, rightNode.endInterval);
-
-//     }
-
-//     public static int[] buildTree(int[] arr,int n){
-//         int N = getNtermsOfTwoPow(n);
-//         System.out.println("N : "+N);
-//         int[] tree = new int[n * 2];
-//         Arrays.fill(tree,Integer.MAX_VALUE);
-//         System.out.println("Building ...");
-//         System.out.println("before : "+Arrays.toString(tree));
-//         int j =0;
-//         for(int i=N/2;i<N;i++){
-//             tree[i] = arr[j];
-//             System.out.println("tree : "+tree[i]+" i : "+i);
-//             j++;
-//         }
-//         for(int i=(N/2)-1;i>0;i--){
-//             int rightChild = tree[2 * i +1];
-//             int leftChild = tree[2*i];
-//             System.out.println("parent : "+i+" right : "+rightChild +" left : "+leftChild);
-//             tree[i] = Math.min(rightChild, leftChild);
-//         }
-//         System.out.println("after : "+Arrays.toString(tree));
-//         return tree;
-//     }
-//     public static int getNtermsOfTwoPow(int n){
-//         return n * 2;
-//     }
-// }
+class Main{
+    public static void main(String[] args) {
+        int[] arrs = {2,3,6,7};
+        Arrays.sort(arrs);
+    }
+    public static void backTracking(int tar,int[] arr,int sum){
+        if(sum > tar){
+            return;
+        }
+        if(sum == tar){
+            System.out.println("FOUNDED : ");
+            return;
+        }
+    }
+}
