6a9779f080c51cf210ed94ce7b836c674c00d309
SK9486 <valli74appan@gmail.com>
2026-08-04 10:38:05 +0530

    396 Rotating function

--- Hello.java
+++ Hello.java
@@ -1,61 +1,41 @@
 import java.util.*;
-
-class Main {
+public class Hello {
     public static void main(String[] args) {
-//         char[][] maze = {
-//     {'+', '+', '.', '+'},
-//     {'.', '.', '.', '+'},
-//     {'+', '+', '+', '.'}
-// };
-char[][] maze = {
-    {'+', '+', '+'},
-    {'.', '.', '.'},
-    {'+', '+', '+'}
-};
-// char[][] maze = {
-//     {'.','+'}
-// };
-        int[] standing_point = {1,0};
-         System.out.println(bfs(standing_point[0], standing_point[1], maze));
-    }
-
-
-    public static int bfs(int x,int y,char[][] maze){
-        int c= 0;
-        System.out.println(maze.length);
-        System.out.println(maze[0].length);
-        Deque<int[]> dq = new LinkedList<>();
-        dq.addLast(new int[] {x,y});
-        int size = 0;
-        while(!dq.isEmpty()){
-            for(int k=0;k<size;k++){
-                int[] pos = dq.removeFirst();
-                int i = pos[0];
-                int j = pos[1];
-                System.out.println(i+" "+j);
-                maze[i][j] = '+';
-                int[][] dirs = {
-                    { i - 1, j },
-                    { i + 1, j },
-                    { i, j - 1 },
-                    { i, j + 1 }
-                };
-            for(int[] dir : dirs){
-                int a = dir[0];
-                int b = dir[1];
-                if(a>=0 && a < maze.length && b>=0 && b<maze[0].length && maze[a][b] == '.'){
-                    c++;
-                    if((a == 0 || a ==  maze.length - 1) || (b == 0 || b == maze[0].length -1) ){
-                         System.out.println("Breaked "+c);
-                         return c;
-                    }
-                    dq.add(new int[] {a,b});
-                }
-            }
-            }
-            size = dq.size();
+        int[] arr = {2,3,6,7};
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        int tar = 7;
+        int sum = 0;
+        backtracker(arr, sum, res,tar,list);
+        System.out.println("res :");
+        for(ArrayList<Integer> ar : res){
+            System.out.println(ar);
         }
-        return -1;
-
     }
-}
+    public static void backtracker(int[] arr,int sum,ArrayList<ArrayList<Integer>> res,int tar,ArrayList<Integer> list){
+        if(sum == tar){
+            // System.out.println("sum == tar");
+            res.add(new ArrayList<>(list));
+            // System.out.println(list);
+            // sum-=dq.removeLast();
+            // backtracker(arr, sum, dq, tar);
+            return;
+        }
+        if(sum > tar){
+            // System.out.println("sum > tar");
+            // sum-=dq.removeLast();
+            //  System.out.println("sum : "+sum);
+            // backtracker(arr, sum, dq, tar);
+            return;
+
+        }
+        // System.out.println(list);
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+            // System.out.println("sum : "+sum);
+            list.addLast(i);
+            backtracker(arr, sum, res, tar,list);
+            sum-=arr[list.removeLast()];
+        }
+    }
+}   
--- Main.java
+++ Main.java
@@ -4,50 +4,33 @@ import java.util.Arrays;
 
 class Main{
     public static void main(String[] args) {
-        String nums = "9223372036854775808";
-        ArrayList<String> base_arr = new ArrayList<>();
-        for(int i=0;i<nums.length();i++){
-            if( i == 0){
-                base_arr = build_base_arr(Character.getNumericValue(nums.charAt(i)),nums.length());
-            }else{
-                updateing_arry(Character.getNumericValue(nums.charAt(i)), i, base_arr);
+        int[] nums = {4,3,2,6};
+        int max_sum = Integer.MIN_VALUE;
+        System.out.println(Arrays.toString(nums));
+        for(int i=1;i<=nums.length;i++){
+            rotateArr(nums);
+            int sum = sumOfArr(nums);
+            System.out.println("Sum : "+sum);
+            if(sum > max_sum){
+                max_sum = sum;
             }
+            System.out.println(Arrays.toString(nums));
         }
-        System.out.println(base_arr);
+        System.out.println(max_sum);
     }
-    public static ArrayList<String> build_base_arr(int n,int len){
-        ArrayList<String> baseArr = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            long ele = (long)Math.pow(10L,len-1);
-            baseArr.add(String.valueOf(ele));
+    public static void rotateArr(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            int last = arr[arr.length -1];
+            int frst = arr[i];
+            arr[arr.length - 1] = frst;
+            arr[i] = last;
         }
-        return baseArr;
     }
-    public static ArrayList<String> updateing_arry(int n,int pos,ArrayList<String> base_arr){
-        for(int i=0;i<n;i++){
-            if(i > base_arr.size() - 1){
-                System.out.println("i : "+i+" n : "+n);
-                addOnes(n-i, base_arr);
-                break;
-            }
-            String curr = base_arr.get(i);
-            String updated = update(curr,pos);
-            base_arr.set(i,updated);
-            System.out.println("curr : "+curr);
+    public static int sumOfArr(int[] arr){
+        int sum = 0;
+        for(int i=0;i<arr.length;i++){
+            sum += (i * arr[i]);
         }
-        return base_arr;
-    }
-
-    public static String update(String str,int pos){
-        char[] chrs = str.toCharArray();
-        chrs[pos] = '1';
-        System.out.println(chrs);
-        return new String(chrs);
-    }
-
-    public static void addOnes(int n,ArrayList<String> baseArr){
-        for(int i=0;i<n;i++){
-            baseArr.add("1");
-        }
+        return sum;
     }
 }
