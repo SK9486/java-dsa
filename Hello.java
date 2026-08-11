11610f2268e0f41a4aff7ba89f648e6cf5fe7246
SK9486 <valli74appan@gmail.com>
2026-08-11 06:12:09 +0530

    skillrack  test code  (rotating the boundary elements in anticlockwise)

--- Hello.java
+++ Hello.java
@@ -1,50 +1,110 @@
 import java.util.*;
 class Hello{
     public static void main(String[] args) {
-       int[] arr = {38,22,52,40,64,46,5,29,60,90};
-       int n = 10;
-       int mul = 4;
-       boolean[] tracker = new boolean[arr.length];
-       System.out.println(Arrays.toString(tracker));
-       for(int i=0;i<arr.length;i++){
-        int ele = arr[i];
-        if(ele % mul == 0){
-            tracker[i] = true;
+        int n = 4;
+        String[][] mat = {
+            {"1","2","3","4"},
+            {"5","6","7","8"},
+            {"9","10","11","12"},
+            {"100","101","102","103"}
+        };
+        // for(int i =0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         mat[i][j] = ""+i+j;
+        //     }
+        // }
+        int k =1;
+        for(int i=0;i<k;i++){
+            int a = 0;
+            int b = n-1;
+             while(a < b){
+                travelsingBoundaryAndRotating(mat, a, b);
+                a++;
+                b--;
         }
-       }
-       System.out.println(Arrays.toString(tracker));
-       int st = -1;
-       int ed = -1;
-       for(int i=0;i<tracker.length;i++){
-        if(tracker[i]){
-            if(st == -1){
-                st = i;
-            }else{
-                ed = i;
-                System.out.println("range : "+st+" "+ed);
-                sortedAndReplace(st, ed, tracker, arr);
-                st=ed;
+        }
+
+    }
+
+    public static void travelsingBoundaryAndRotating(String[][] mat,int a,int b){
+        ArrayList<String> arr = new ArrayList<>();
+        for(int j=a;j<=b;j++){
+            System.out.print(mat[a][j]+" ");
+            arr.add(mat[a][j]);
+        }
+        System.out.println();
+        for(int i=a;i<=b;i++){
+            System.out.print(mat[i][b]+" ");
+            if(i!=a){
+                arr.add(mat[i][b]);
             }
         }
-       }
-       System.out.println(Arrays.toString(arr));
-       System.out.println(Arrays.toString(tracker));
+        System.out.println();
+        for(int j=b;j>=a;j--){
+            System.out.print(mat[b][j]+" ");
+            if(j!=b){
+                arr.add(mat[b][j]);
+            }
+        }
+        System.out.println();
+        for(int j=b;j>=a;j--){
+            System.out.print(mat[j][a]+" ");
+            if(j!=b && j!=a){
+                arr.add(mat[j][a]);
+            }
+        }
+        System.out.println();
+        System.out.println(arr);
+        rotateAndReplace(mat, a, b, arr);
     }
-    public static void sortedAndReplace(int i,int j,boolean[] track,int[] arr){
-        int m = (j-i)-1;
-        int[] splt = new int[m];
-        int x =  i+1;
-        for(int a=0;a<splt.length;a++){
-            splt[a] = arr[x];
-            x++;
+    
+    public static void rotateAndReplace(String[][] mat,int a,int b,ArrayList<String> arr){
+        String fst = arr.get(0);
+        arr.removeFirst();
+        arr.addLast(fst);
+        System.out.println(arr);
+        int idx = 0;
+        for(int j=a;j<=b;j++){
+            System.out.print(mat[a][j]+" ");
+            // arr.add(mat[a][j]);
+            mat[a][j] = arr.get(idx);
+            idx++;
         }
-        Arrays.sort(splt);
-        x=0;
-        for(int a=i+1;a<j;a++){
-            arr[a] = splt[x];
-            x++;
+        System.out.println();
+        for(int i=a;i<=b;i++){
+            System.out.print(mat[i][b]+" ");
+            if(i!=a){
+                // arr.add(mat[i][b]);
+                mat[i][b] = arr.get(idx);
+                idx++;
+            }
         }
-        System.out.println(Arrays.toString(splt));
-        System.out.println(Arrays.toString(arr));
+        System.out.println();
+        for(int j=b;j>=a;j--){
+            System.out.print(mat[b][j]+" ");
+            if(j!=b){
+                // arr.add(mat[b][j]);
+                mat[b][j] = arr.get(idx);
+                idx++;
+            }
+        }
+        System.out.println();
+        for(int j=b;j>=a;j--){
+            System.out.print(mat[j][a]+" ");
+            if(j!=b && j!=a){
+                // arr.add(mat[j][a]);
+                mat[j][a] = arr.get(idx);
+                idx++;
+            }
+        }
+        System.out.println();
+        printArr(mat);
+    }
+
+    public static void printArr(String[][] str){
+        for(String[] strs : str){
+            System.out.println(Arrays.toString(strs));
+        }
+        System.out.println();
     }
 }
