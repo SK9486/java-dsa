28760e68defd5ef3f6fec2ddaa0540e39999b82d
SK9486 <valli74appan@gmail.com>
2026-08-15 10:42:07 +0530

    zoho count from 1 to N

--- Hello.java
+++ Hello.java
@@ -1,110 +1,32 @@
-import java.util.*;
+import java.util.ArrayList;
+
 class Hello{
     public static void main(String[] args) {
-        int n = 4;
-        String[][] mat = {
-            {"1","2","3","4"},
-            {"5","6","7","8"},
-            {"9","10","11","12"},
-            {"100","101","102","103"}
-        };
-        // for(int i =0;i<n;i++){
-        //     for(int j=0;j<n;j++){
-        //         mat[i][j] = ""+i+j;
-        //     }
-        // }
-        int k =1;
-        for(int i=0;i<k;i++){
-            int a = 0;
-            int b = n-1;
-             while(a < b){
-                travelsingBoundaryAndRotating(mat, a, b);
-                a++;
-                b--;
+        int n = 7;
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            arr.add(i);
         }
-        }
-
-    }
-
-    public static void travelsingBoundaryAndRotating(String[][] mat,int a,int b){
-        ArrayList<String> arr = new ArrayList<>();
-        for(int j=a;j<=b;j++){
-            System.out.print(mat[a][j]+" ");
-            arr.add(mat[a][j]);
-        }
-        System.out.println();
-        for(int i=a;i<=b;i++){
-            System.out.print(mat[i][b]+" ");
-            if(i!=a){
-                arr.add(mat[i][b]);
-            }
-        }
-        System.out.println();
-        for(int j=b;j>=a;j--){
-            System.out.print(mat[b][j]+" ");
-            if(j!=b){
-                arr.add(mat[b][j]);
-            }
-        }
-        System.out.println();
-        for(int j=b;j>=a;j--){
-            System.out.print(mat[j][a]+" ");
-            if(j!=b && j!=a){
-                arr.add(mat[j][a]);
-            }
-        }
-        System.out.println();
         System.out.println(arr);
-        rotateAndReplace(mat, a, b, arr);
+        int i =0;
+        int si = arr.size();
+        System.out.println("si : "+si);
+        while(arr.size() > 1){
+            if(i == 0){
+                arr= removeOdd(arr);
+                System.out.println(arr);
+            }else{
+                break;
+            }
+            i++;
+        }
     }
-    
-    public static void rotateAndReplace(String[][] mat,int a,int b,ArrayList<String> arr){
-        String fst = arr.get(0);
-        arr.removeFirst();
-        arr.addLast(fst);
-        System.out.println(arr);
-        int idx = 0;
-        for(int j=a;j<=b;j++){
-            System.out.print(mat[a][j]+" ");
-            // arr.add(mat[a][j]);
-            mat[a][j] = arr.get(idx);
-            idx++;
-        }
-        System.out.println();
-        for(int i=a;i<=b;i++){
-            System.out.print(mat[i][b]+" ");
-            if(i!=a){
-                // arr.add(mat[i][b]);
-                mat[i][b] = arr.get(idx);
-                idx++;
+    public static ArrayList<Integer> removeOdd(ArrayList<Integer> arr){
+        for(int i=0;i<arr.size();i++){
+            if(i % 2 != 0){
+                arr.remove(i);
             }
         }
-        System.out.println();
-        for(int j=b;j>=a;j--){
-            System.out.print(mat[b][j]+" ");
-            if(j!=b){
-                // arr.add(mat[b][j]);
-                mat[b][j] = arr.get(idx);
-                idx++;
-            }
-        }
-        System.out.println();
-        for(int j=b;j>=a;j--){
-            System.out.print(mat[j][a]+" ");
-            if(j!=b && j!=a){
-                // arr.add(mat[j][a]);
-                mat[j][a] = arr.get(idx);
-                idx++;
-            }
-        }
-        System.out.println();
-        printArr(mat);
-    }
-
-    public static void printArr(String[][] str){
-        for(String[] strs : str){
-            System.out.println(Arrays.toString(strs));
-        }
-        System.out.println();
+        return arr;
     }
 }
--- Main.java
+++ Main.java
@@ -3,30 +3,64 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 class Main{
-    public static void main(String[] args) {
-        int[] nums = {1,1,1};
-        ArrayList<Integer> arr = new ArrayList<>();
-        for(int a : nums){
-            arr.add(a);
-            arr.add(-a);
-        }
-        ArrayList<Integer> list = new ArrayList<>();
-        backtrack(arr, list, 1, 0,nums.length);
-        System.out.println(arr);
-    }
-    public static void backtrack(ArrayList<Integer>  arr,ArrayList<Integer> list,int tar,int sum,int len){
-        if(list.size() == len){
-            // System.out.println(list);
-            if(sum == tar){
-                System.out.println(list);
-            }
-            return;
-        }
-        for(int a : arr){
-            list.addLast(a);
-            sum+=a;
-            backtrack(arr, list, tar, sum,len);
-            sum-=list.removeLast();
-        }
-    }
+   public static void main(String[] args) {
+      long sum =0;
+      long n = 9999999;
+      for(long i=1;i<=n;i++){
+         if(i == 1 && n>=9){
+            i+=9 * 1;
+            sum+=9;
+            System.out.println(sum);
+         }
+         if(i == 10 && n>=99){
+            i+=90;
+            sum += 90 * 2;
+            System.out.println(90 * 2);
+            System.out.println("i : "+i);
+            System.out.println(sum);
+         }
+         if( i == 100 && n >= 999){
+            i+=900;
+            sum += 900 * 3;
+            System.out.println(900 * 3);
+             System.out.println("i : "+i);
+            System.out.println(sum);
+         }
+         if(i == 1000 && n >= 9999){
+            i+=9000;
+            sum += 9000 * 4;
+            System.out.println(9000 * 4);
+            System.out.println("i : "+i);
+            System.out.println(sum);
+         }
+         if(i == 10000 && n >= 99999){
+            i+=90000;
+            sum += 90000 * 5;
+            System.out.println(90000 * 5);
+             System.out.println("i : "+i);
+            System.out.println(sum);
+         }
+         if(i == 100000 && n >= 999999){
+            i+=900000;
+            sum += 900000 * 6;
+            System.out.println(900000 * 6);
+             System.out.println("i : "+i);
+            System.out.println(sum);
+         }
+         if(i == 1000000 && n >= 9999999){
+            i+=9000000;
+            sum += 9000000 * 7;
+            System.out.println(9000000 * 7);
+            System.out.println("i : "+i);
+            System.out.println(sum);
+         }
+         System.out.println(" i "+i);
+         if(i <= n){
+            String st_i = String.valueOf(i);
+            System.out.println("st_i : "+st_i);
+            sum += st_i.length();
+         }
+      }
+      System.out.println(sum);
+   }
 }
