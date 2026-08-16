2e7b58156f95b1e7b15188a989a02f9738803d8c
SK9486 <valli74appan@gmail.com>
2026-08-16 11:18:25 +0530

    zoho decode ways

--- Main.java
+++ Main.java
@@ -4,63 +4,38 @@ import java.util.Arrays;
 
 class Main{
    public static void main(String[] args) {
-      long sum =0;
-      long n = 9999999;
-      for(long i=1;i<=n;i++){
-         if(i == 1 && n>=9){
-            i+=9 * 1;
-            sum+=9;
-            System.out.println(sum);
-         }
-         if(i == 10 && n>=99){
-            i+=90;
-            sum += 90 * 2;
-            System.out.println(90 * 2);
-            System.out.println("i : "+i);
-            System.out.println(sum);
-         }
-         if( i == 100 && n >= 999){
-            i+=900;
-            sum += 900 * 3;
-            System.out.println(900 * 3);
-             System.out.println("i : "+i);
-            System.out.println(sum);
-         }
-         if(i == 1000 && n >= 9999){
-            i+=9000;
-            sum += 9000 * 4;
-            System.out.println(9000 * 4);
-            System.out.println("i : "+i);
-            System.out.println(sum);
-         }
-         if(i == 10000 && n >= 99999){
-            i+=90000;
-            sum += 90000 * 5;
-            System.out.println(90000 * 5);
-             System.out.println("i : "+i);
-            System.out.println(sum);
-         }
-         if(i == 100000 && n >= 999999){
-            i+=900000;
-            sum += 900000 * 6;
-            System.out.println(900000 * 6);
-             System.out.println("i : "+i);
-            System.out.println(sum);
-         }
-         if(i == 1000000 && n >= 9999999){
-            i+=9000000;
-            sum += 9000000 * 7;
-            System.out.println(9000000 * 7);
-            System.out.println("i : "+i);
-            System.out.println(sum);
-         }
-         System.out.println(" i "+i);
-         if(i <= n){
-            String st_i = String.valueOf(i);
-            System.out.println("st_i : "+st_i);
-            sum += st_i.length();
-         }
+      int[] arr = {1,2,3};
+      ArrayList<String> res = new ArrayList<>();
+      res.add(String.valueOf(arr[0]));
+      recursion(1, res, arr);
+   }
+
+   public static void recursion(int a,ArrayList<String> res,int[] arr){
+      ArrayList<String> arr1 = new ArrayList<>(res);
+      ArrayList<String> arr2 = new ArrayList<>(res);
+      if(a == arr.length){
+         // System.out.println("A == arr len");
+         System.out.println(res);
+         return;
       }
-      System.out.println(sum);
+      int curr = arr[a];
+      if(curr < 1 || curr > 26){
+         return;
+      }
+      // single
+      arr1.add(String.valueOf(curr));
+      // System.out.println(arr1);
+      recursion(a+1, arr1, arr);
+      // mingle
+      String prev = arr2.removeLast();
+      // System.out.println("prev : "+prev);
+      String merge = prev+curr;
+      int val = Integer.valueOf(merge);
+      if(val < 1 || val > 26){
+         return;
+      }
+      arr2.add(merge);
+      // System.out.println(arr2);
+      recursion(a+1, arr2, arr);
    }
 }
