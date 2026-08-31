f219b5a0ded7d9df7a553f5c51d6ffc076837630
SK9486 <valli74appan@gmail.com>
2026-08-31 11:06:04 +0530

    Edit distance leetcode DP

--- Main.java
+++ Main.java
@@ -5,47 +5,37 @@ import java.util.Scanner;
 
 class Main {
    public static void main(String[] args) {
-      String text1 = "bl";
-      String text2 = "yby";
-      // String text1 = "abcde";
-      // String text2 = "ace";
-      text1 = "abc";
-      text2 = "abc";
-      int n = text2.length() + 1;
-      int m = text1.length() + 1;
-      int[] row = new int[m];
-      for (int i = 0; i < n; i++) {
-         int[] old = row.clone();
-         for (int j = 0; j < m; j++) {
-            if (i != 0 && j != 0) {
-               char ch1 = text2.charAt(i - 1);
-               char ch2 = text1.charAt(j - 1);
-               // System.out.println("j : "+ch2+" i : "+ch1);
-               if (ch1 == ch2) {
-                  // System.out.println("MATCHES : "+ch1+" "+ch2);
-                  // System.out.println("matches old prev value + 1");
-                  row[j] = old[j - 1] + 1;
-                  // System.out.println(Arrays.toString(old));
-                  // System.out.println(Arrays.toString(row));
-               } else {
-                  // System.out.println("max of prev and old value");
-                  System.out.println("before");
-                  System.out.println(Arrays.toString(old));
-                  System.out.println(Arrays.toString(row));
-                  System.out.println("procced");
-                  row[j] = Math.max(row[j - 1], old[j]);
-                  // System.out.println("j : " + j);
-                  // System.out.println("row[j-1] : " + row[j - 1]);
-                  // System.out.println("old[j] : " + old[j]);
-                  System.out.println("after");
-                  System.out.println(Arrays.toString(old));
-                  System.out.println(Arrays.toString(row));
-                  System.out.println();
-
-               }
+      String s1 = "abdc";
+      String s2 = "acd";
+      int n = s1.length();
+      int m = s2.length();
+      int[][] mat = new int[n + 1][m + 1];
+      for (int i = 0; i < n + 1; i++) {
+         mat[i][m] = n - i;
+         System.out.println(mat[i][m]);
+      }
+      for (int j = 0; j < m + 1; j++) {
+         mat[n][j] = m - j;
+         System.out.println(mat[n][j]);
+      }
+      for (int i = n - 1; i >= 0; i--) {
+         for (int j = m - 1; j >= 0; j--) {
+            char ch1 = s1.charAt(i);
+            char ch2 = s2.charAt(j);
+            if (ch1 == ch2) {
+               mat[i][j] = mat[i + 1][j + 1];
+            } else {
+               int ifReplace = mat[i + 1][j + 1];
+               int ifDeleted = mat[i + 1][j];
+               int ifInserted = mat[i][j + 1];
+               mat[i][j] = 1 + Math.min(Math.min(ifReplace, ifDeleted), ifInserted);
             }
          }
-         System.out.println(Arrays.toString(row));
+         System.out.println();
       }
+      for (int[] ar : mat) {
+         System.out.println(Arrays.toString(ar));
+      }
+
    }
 }
