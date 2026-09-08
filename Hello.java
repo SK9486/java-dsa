ffc335cf1f8726d0e32f9b7794a2501f270ccc1f
SK9486 <valli74appan@gmail.com>
2026-09-08 12:30:14 +0530

    C. 101

--- Hello.java
+++ Hello.java
@@ -1,37 +1,64 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
+
 import java.util.Scanner;
 
 public class Hello {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int m = sc.nextInt();
+        int x = sc.nextInt();
         sc.nextLine();
-        while (m != 0) {
+        while (x != 0) {
             int n = sc.nextInt();
-            int k = sc.nextInt();
             sc.nextLine();
             String str = sc.nextLine();
-            // System.out.println(n + " " + k + " " + str);
-            System.out.println(ifOwns(str, n, k));
-            m--;
+            // System.out.println("n : " + n + " " + str);
+            processInputs(str);
+            x--;
         }
-
     }
 
-    public static int ifOwns(String str, int n, int k) {
-        int c = 0;
+    public static void processInputs(String str) {
+        String[] inps = str.split(" ");
+        // System.out.println(Arrays.toString(inps));
+        int n = inps.length;
+        int first = -1;
         for (int i = 0; i < n; i++) {
-            // System.out.println("st : " + i + " ed : " + (i + k));
-            String sub = str.substring(i, i + k);
-            // System.out.println("sub : " + sub);
-            if (!sub.contains("0")) {
-                // System.out.println("noting own");
-                c++;
+            String ch = inps[i];
+            if (ch.equals("-1") || ch.equals("1")) {
+                first = i;
+                break;
             }
-            i += k - 1;
         }
-        return c;
+        int last = -1;
+        for (int i = n - 1; i >= 0; i--) {
+            String ch = inps[i];
+            if (ch.equals("-1") || ch.equals("1")) {
+                last = i;
+                break;
+            }
+        }
+        // System.out.println("first : " + first);
+        // System.out.println("last : " + last);
+        if (first != -1 && last != -1) {
+            for (int i = 0; i < n; i++) {
+                String st = inps[i];
+                if (i >= first && i <= last) {
+                    if (i == first || i == last) {
+                        if (st.equals("-1")) {
+                            inps[i] = "1";
+                        }
+                    } else {
+                        if (st.equals("-1")) {
+                            inps[i] = "0";
+                        }
+                    }
+                }
+            }
+        }
+        String res = "";
+        for (String st : inps) {
+            res += (st + " ");
+        }
+        System.out.println(res);
     }
+
 }
