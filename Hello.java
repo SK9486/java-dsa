30b694532459ef57d301b77e71963225180c2b61
SK9486 <valli74appan@gmail.com>
2026-09-08 11:08:28 +0530

    A. Moo Language School

--- Hello.java
+++ Hello.java
@@ -1,40 +1,37 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
 
-class Hello {
+public class Hello {
     public static void main(String[] args) {
-        int[][] matx = {
-                { 1, 2, 3, 4, 5, 6 },
-                { 1, 2, 3, 4, 5, 6 },
-                { 1, 2, 3, 4, 5, 6 },
-                { 1, 2, 3, 4, 5, 6 },
-                { 1, 2, 3, 4, 5, 6 },
-                { 1, 2, 3, 4, 5, 6 },
-        };
-        // int[][] matx = {
-        // { 11, 12, 13 },
-        // { 21, 22, 23 },
-        // { 31, 32, 33 }
-        // };
-        int n = matx.length;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == j) {
-                    printRem(i, j, matx);
-                    break;
-                } else {
-                    System.out.print(matx[i][j] + " ");
-                }
-            }
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        sc.nextLine();
+        while (m != 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            sc.nextLine();
+            String str = sc.nextLine();
+            // System.out.println(n + " " + k + " " + str);
+            System.out.println(ifOwns(str, n, k));
+            m--;
         }
 
     }
 
-    public static void printRem(int i, int j, int[][] matx) {
-        for (int a = i; a >= 0; a--) {
-            System.out.print(matx[a][j] + " ");
+    public static int ifOwns(String str, int n, int k) {
+        int c = 0;
+        for (int i = 0; i < n; i++) {
+            // System.out.println("st : " + i + " ed : " + (i + k));
+            String sub = str.substring(i, i + k);
+            // System.out.println("sub : " + sub);
+            if (!sub.contains("0")) {
+                // System.out.println("noting own");
+                c++;
+            }
+            i += k - 1;
         }
-        System.out.println();
-        // System.out.println("printRem");
+        return c;
     }
 }
--- Main.java
+++ Main.java
@@ -1,29 +1,42 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-class Main {
+public class Main {
    public static void main(String[] args) {
-      int[][] matx = {
-            { 11, 12, 13 },
-            { 21, 22, 23 },
-            { 31, 32, 33 }
-      };
-      int n = matx.length;
-      for (int a = 0; a < n; a++) {
-         int i = a;
-         int j = a;
-         while (i >= 0 || i < n) {
-            System.out.println(i + " " + j + " : " + matx[i][j]);
-            i--;
-         }
-         i = a;
-         while (j >= 0 || j < n) {
-            System.out.println(i + " " + j + " : " + matx[i][j]);
-            j--;
-         }
+      // int[] arr = { 48, 541, 23, 68, 13, 41, 6 };
+      // int n = 7;
+      // int m = 3;
+      int[] arr = { 1, -3, -44, 2, 77 };
+      int n = 5;
+      int m = 2;
+      int st = 0;
+      int ed = st + (m - 1);
+      while (ed < n) {
+         selectionSort(st, ed + 1, arr);
+         System.out.println("st : " + st + " ed : " + ed);
+         System.out.println(Arrays.toString(arr));
+         st = ed;
+         st++;
+         ed += (m);
       }
 
    }
+
+   public static void selectionSort(int s, int e, int[] arr) {
+      for (int i = s; i < e; i++) {
+         int temp = arr[i];
+         int maxIdx = -1;
+         int max = Integer.MIN_VALUE;
+         for (int j = i; j < e; j++) {
+            if (arr[j] > max) {
+               max = arr[j];
+               maxIdx = j;
+            }
+         }
+         arr[i] = max;
+         arr[maxIdx] = temp;
+         System.out.println("i : " + i + " min : " + max);
+      }
+   }
+
 }
