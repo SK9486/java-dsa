6fc5b2ecfd15323d92eb6a596e4bb9fbc4875b23
SK9486 <valli74appan@gmail.com>
2026-09-09 06:58:35 +0530

    Subset Sum Problem

--- Hello.java
+++ Hello.java
@@ -1,64 +1,40 @@
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Hello {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        sc.nextLine();
-        while (x != 0) {
-            int n = sc.nextInt();
-            sc.nextLine();
-            String str = sc.nextLine();
-            // System.out.println("n : " + n + " " + str);
-            processInputs(str);
-            x--;
-        }
-    }
-
-    public static void processInputs(String str) {
-        String[] inps = str.split(" ");
-        // System.out.println(Arrays.toString(inps));
-        int n = inps.length;
-        int first = -1;
-        for (int i = 0; i < n; i++) {
-            String ch = inps[i];
-            if (ch.equals("-1") || ch.equals("1")) {
-                first = i;
-                break;
-            }
-        }
-        int last = -1;
-        for (int i = n - 1; i >= 0; i--) {
-            String ch = inps[i];
-            if (ch.equals("-1") || ch.equals("1")) {
-                last = i;
-                break;
-            }
-        }
-        // System.out.println("first : " + first);
-        // System.out.println("last : " + last);
-        if (first != -1 && last != -1) {
-            for (int i = 0; i < n; i++) {
-                String st = inps[i];
-                if (i >= first && i <= last) {
-                    if (i == first || i == last) {
-                        if (st.equals("-1")) {
-                            inps[i] = "1";
-                        }
-                    } else {
-                        if (st.equals("-1")) {
-                            inps[i] = "0";
+        int[] arr = { 2, 3, 1 };
+        int tar = 1;
+        boolean[] dp = new boolean[tar + 1];
+        for (int i = 0; i < arr.length; i++) {
+            int curr = arr[i];
+            if (curr <= tar) {
+                if (i == 0) {
+                    dp[0] = true;
+                    int sum = curr + 0;
+                    if (sum <= tar) {
+                        dp[sum] = true;
+                    }
+                } else {
+                    ArrayList<Integer> idexs = new ArrayList<>();
+                    for (int j = 0; j < dp.length; j++) {
+                        if (dp[j]) {
+                            int sum = j + curr;
+                            if (sum <= tar) {
+                                idexs.add(sum);
+                            }
                         }
                     }
+                    for (int a : idexs) {
+                        dp[a] = true;
+                    }
+                    idexs.clear();
                 }
+                dp[curr] = true;
             }
         }
-        String res = "";
-        for (String st : inps) {
-            res += (st + " ");
-        }
-        System.out.println(res);
+        System.out.println(Arrays.toString(dp));
     }
-
 }
--- Main.java
+++ Main.java
@@ -3,40 +3,36 @@ import java.util.Scanner;
 
 public class Main {
    public static void main(String[] args) {
-      // int[] arr = { 48, 541, 23, 68, 13, 41, 6 };
-      // int n = 7;
-      // int m = 3;
-      int[] arr = { 1, -3, -44, 2, 77 };
-      int n = 5;
-      int m = 2;
-      int st = 0;
-      int ed = st + (m - 1);
-      while (ed < n) {
-         selectionSort(st, ed + 1, arr);
-         System.out.println("st : " + st + " ed : " + ed);
-         System.out.println(Arrays.toString(arr));
-         st = ed;
-         st++;
-         ed += (m);
-      }
+      String inp = "q^z^zthere^z^y^yyou";
+      StringBuilder sb = new StringBuilder();
+      StringBuilder rem = new StringBuilder();
+      for (int i = 0; i < inp.length(); i++) {
+         char ch = inp.charAt(i);
+         if (ch == '^') {
+            i++;
+            char nxt = inp.charAt(i);
+            if (nxt == 'z' && sb.toString().length() > 0) {
+               System.out.println("undo");
+               int n = sb.toString().length();
+               char last = sb.toString().charAt(n - 1);
+               rem.append(last);
+               sb.deleteCharAt(n - 1);
 
-   }
-
-   public static void selectionSort(int s, int e, int[] arr) {
-      for (int i = s; i < e; i++) {
-         int temp = arr[i];
-         int maxIdx = -1;
-         int max = Integer.MIN_VALUE;
-         for (int j = i; j < e; j++) {
-            if (arr[j] > max) {
-               max = arr[j];
-               maxIdx = j;
             }
-         }
-         arr[i] = max;
-         arr[maxIdx] = temp;
-         System.out.println("i : " + i + " min : " + max);
-      }
-   }
+            if (nxt == 'y' && rem.toString().length() > 0) {
+               System.out.println("redo");
+               int n = rem.toString().length();
+               char last = rem.toString().charAt(n - 1);
+               sb.append(last);
+               rem.deleteCharAt(n - 1);
 
+            }
+         } else {
+            System.out.println(ch);
+            sb.append(ch);
+         }
+         System.out.println("sb : " + sb.toString());
+      }
+      System.out.println(sb.toString());
+   }
 }
