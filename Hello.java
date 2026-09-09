70ad955d9791a6d9e8ed393f333569f082f0e7de
SK9486 <valli74appan@gmail.com>
2026-09-09 11:56:34 +0530

    skillrack minDays of iceCream recursion

--- Hello.java
+++ Hello.java
@@ -5,36 +5,45 @@ import java.util.Scanner;
 
 public class Hello {
     public static void main(String[] args) {
-        int[] arr = { 2, 3, 1 };
-        int tar = 1;
-        boolean[] dp = new boolean[tar + 1];
+        int[] arr = { 3, 3, 3, 3 };
+        int[] c = { Integer.MAX_VALUE };
+        recursion(arr, 0, c);
+        System.out.println("final : " + c[0]);
+    }
+
+    public static void recursion(int[] arr, int i, int[] c) {
+        if (i == arr.length) {
+            System.out.println(Arrays.toString(arr));
+            int c1 = findMinDays(0, arr);
+            c[0] = Math.min(c[0], c1);
+            return;
+        }
+        int curr = arr[i];
+        if (curr == 3) {
+            int[] arr1 = Arrays.copyOf(arr, arr.length);
+            arr1[i] = 1;
+            int[] arr2 = Arrays.copyOf(arr, arr.length);
+            arr2[i] = 2;
+            recursion(arr1, i + 1, c);
+            recursion(arr2, i + 1, c);
+        } else {
+            recursion(arr, i + 1, c);
+        }
+    }
+
+    public static int findMinDays(int prev, int[] arr) {
+        int c = 0;
         for (int i = 0; i < arr.length; i++) {
             int curr = arr[i];
-            if (curr <= tar) {
-                if (i == 0) {
-                    dp[0] = true;
-                    int sum = curr + 0;
-                    if (sum <= tar) {
-                        dp[sum] = true;
-                    }
-                } else {
-                    ArrayList<Integer> idexs = new ArrayList<>();
-                    for (int j = 0; j < dp.length; j++) {
-                        if (dp[j]) {
-                            int sum = j + curr;
-                            if (sum <= tar) {
-                                idexs.add(sum);
-                            }
-                        }
-                    }
-                    for (int a : idexs) {
-                        dp[a] = true;
-                    }
-                    idexs.clear();
-                }
-                dp[curr] = true;
+            if (i > 0) {
+                prev = arr[i - 1];
+            }
+            if (curr == 0 || prev == curr) {
+                c++;
+                arr[i] = 0;
             }
         }
-        System.out.println(Arrays.toString(dp));
+        System.out.println("c : " + c);
+        return c;
     }
 }
