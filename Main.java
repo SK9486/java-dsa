bc84cbba11c474b515e15ecf0e88cb2a1df8f9e0
SK9486 <valli74appan@gmail.com>
2026-09-11 05:58:39 +0530

    Skillrack DP min lan cost sum

--- Main.java
+++ Main.java
@@ -12,58 +12,97 @@ public class Main {
       int[] lane1_cost = { 0, 1, 2, 3, 4 };
       int[] lane2_cost = { 0, 2, 3, 2, 1 };
       int[] switching_cost = { 0, 1, 2, 1, 2 };
-      int[] dp = new int[no_of_stations + 1];
+      // int[] dp = new int[no_of_stations + 1];
+      int[][] dp = new int[no_of_stations + 1][2];
       int[] prev_path = new int[no_of_stations + 1];
       prev_path[0] = -1;
       printDatas(lane1_cost, lane2_cost, switching_cost, dp, prev_path, no_of_stations);
       int cost_for_path1 = -1;
       int cost_for_path2 = -1;
+      int cost_with_dp_path1 = -1;
+      int cost_with_dp_path2 = -1;
       for (int i = 1; i <= no_of_stations; i++) {
 
          int prev_path_of_car = prev_path[i - 1];
          if (prev_path_of_car == -1) {
-            System.out.println("intial state of car");
-            cost_for_path1 = lane1_cost[i] + switching_cost[i];
-            cost_for_path2 = lane2_cost[i] + switching_cost[i];
+            System.out.println("initial state of car");
+            // cost_for_path1 = lane1_cost[i] + switching_cost[i];
+            // cost_for_path2 = lane2_cost[i] + switching_cost[i];
+            // cost_with_dp_path1 = dp[i - 1][0] + cost_for_path1;
+            // cost_with_dp_path2 = dp[i - 1][1] + cost_for_path2;
          } else if (prev_path_of_car == 1) {
             System.out.println("car currently on lan 1");
-            cost_for_path1 = lane1_cost[i];
-            cost_for_path2 = lane2_cost[i] + switching_cost[i];
+            // cost_for_path1 = lane1_cost[i];
+            // cost_for_path2 = lane2_cost[i] + switching_cost[i];
+            // cost_with_dp_path1 = dp[i - 1][0] + cost_for_path1;
+            // cost_with_dp_path2 = dp[i - 1][0] + cost_for_path2;
          } else if (prev_path_of_car == 2) {
             System.out.println("car currently on lan 2");
-            cost_for_path1 = lane1_cost[i] + switching_cost[i];
-            cost_for_path2 = lane2_cost[i];
+            // cost_for_path1 = lane1_cost[i] + switching_cost[i];
+            // cost_for_path2 = lane2_cost[i];
+            // cost_with_dp_path1 = dp[i - 1][1] + cost_for_path1;
+            // cost_with_dp_path2 = dp[i - 1][1] + cost_for_path2;
+
          } else if (prev_path_of_car == 3) {
             System.out.println("you can choose both lanes");
-            cost_for_path1 = lane1_cost[i] + switching_cost[i];
-            cost_for_path2 = lane2_cost[i] + switching_cost[i];
+            // cost_for_path1 = lane1_cost[i] + switching_cost[i];
+            // cost_for_path2 = lane2_cost[i] + switching_cost[i];
+            // cost_with_dp_path1 = dp[i - 1][0] + cost_for_path1;
+            // cost_with_dp_path2 = dp[i - 1][1] + cost_for_path2;
          } else {
             System.out.println("invalid ");
             break;
          }
-         System.out.println("cost1 : " + cost_for_path1);
-         System.out.println("cost2 : " + cost_for_path2);
-         int min_final_cost = Math.min(cost_for_path1, cost_for_path2);
-         System.out.println("min cost : " + min_final_cost);
-         // UPDATING THE DP
          int chosen_path = -1;
-         dp[i] = dp[i - 1] + min_final_cost;
-         if (cost_for_path1 < cost_for_path2) {
+         if (cost_with_dp_path1 < cost_with_dp_path2) {
+            // dp[i][0] = Math.min(cost_with_dp_path1, cost_for_path2);
             chosen_path = 1;
-         } else if (cost_for_path2 < cost_for_path1) {
+         } else if (cost_with_dp_path2 < cost_with_dp_path1) {
+            // dp[i][1] = Math.min(cost_with_dp_path1, cost_for_path2);
             chosen_path = 2;
          } else {
+            // dp[i][0] = Math.min(cost_with_dp_path1, cost_for_path2);
+            ;
+            // dp[i][1] = Math.min(cost_with_dp_path1, cost_for_path2);
+            ;
             chosen_path = 3;
          }
+         System.out.println("cost1 : " + cost_for_path1);
+         System.out.println("cost2 : " + cost_for_path2);
+
+         dp[i][0] = Math.min(dp[i - 1][0] + lane1_cost[i],
+               dp[i - 1][1] + switching_cost[i] + lane1_cost[i]);
+         dp[i][1] = Math.min(dp[i - 1][0] + lane2_cost[i],
+               dp[i - 1][1] + switching_cost[i] + lane2_cost[i]);
+
+         // int min_final_cost = Math.min(cost_for_path1, cost_for_path2);
+         // System.out.println("min cost : " + min_final_cost);
+
+         // UPDATING THE DP
+         // int chosen_path = -1;
+         // // dp[i] = dp[i - 1] + min_final_cost;
+         // if (cost_for_path1 < cost_for_path2) {
+         // chosen_path = 1;
+         // dp[i][0] = dp[i - 1][0] + cost_for_path1;
+         // } else if (cost_for_path2 < cost_for_path1) {
+         // chosen_path = 2;
+         // dp[i][1] = dp[i - 1][1] + cost_for_path2;
+         // } else {
+         // chosen_path = 3;
+         // dp[i][0] = dp[i - 1][0] + cost_for_path1;
+         // dp[i][1] = dp[i - 1][1] + cost_for_path2;
+         // }
          // UPDATING THE PATH
          prev_path[i] = chosen_path;
          System.out.println(" i : " + i);
       }
-      printDatas(lane1_cost, lane2_cost, switching_cost, dp, prev_path, no_of_stations);
-      System.out.println("final min cost : " + dp[no_of_stations]);
+      printDatas(lane1_cost, lane2_cost, switching_cost, dp, prev_path,
+            no_of_stations);
+      int min_final_cost = Math.min(dp[no_of_stations][0], dp[no_of_stations][1]);
+      System.out.println("final min cost : " + min_final_cost);
    }
 
-   public static void printDatas(int[] lane1, int[] lane2, int[] cost, int[] dp, int[] paths, int n) {
+   public static void printDatas(int[] lane1, int[] lane2, int[] cost, int[][] dps, int[] paths, int n) {
       System.out.println("lane 1 cost");
       System.out.println(Arrays.toString(lane1));
       System.out.println("lane 2 cost");
@@ -71,7 +110,10 @@ public class Main {
       System.out.println("cost ");
       System.out.println(Arrays.toString(cost));
       System.out.println("dp");
-      System.out.println(Arrays.toString(dp));
+      // System.out.println(Arrays.toString(dp));
+      for (int[] dp : dps) {
+         System.out.println(Arrays.toString(dp));
+      }
       System.out.println("paths");
       System.out.println(Arrays.toString(paths));
 
