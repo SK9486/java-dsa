339e602c9d1823c23a44360c58c257fc08eaed59
SK9486 <valli74appan@gmail.com>
2026-09-11 14:17:18 +0530

    C - Write and Erase

--- Hello.java
+++ Hello.java
@@ -1,39 +1,25 @@
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Hello {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int m = 3;
-        int[][] matx = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                matx[i][j] = sc.nextInt();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        while (n != 0) {
+            int curr = sc.nextInt();
+            map.put(curr, map.getOrDefault(curr, 0) + 1);
+            n--;
+        }
+        int c = 0;
+        for (Integer key : map.keySet()) {
+            // System.out.println(key + " " + map.get(key));
+            if (map.get(key) % 2 != 0) {
+                c++;
             }
         }
-        int[] c = { 0 };
-        recursion(matx, 0, 0, c, m);
-        System.out.println(c[0]);
-        // for (int[] ar : matx) {
-        // System.out.println(Arrays.toString(ar));
-        // }
-    }
-
-    public static void recursion(int[][] matx, int i, int prev, int[] c, int m) {
-        if (i == m) {
-            // System.out.println("prev :" + prev);
-            c[0]++;
-            return;
-        }
-        int[] arr = matx[i];
-        for (int j = 0; j < arr.length; j++) {
-            int curr = arr[j];
-            if (curr > prev) {
-                recursion(matx, i + 1, curr, c, m);
-            } else {
-                return;
-            }
-        }
+        System.out.println(c);
     }
 }
